c34a38621724e1a98578ce66c3ec04d92b300291
Varun Agarwal <165192602+Agarwalvarun23@users.noreply.github.com>
2025-06-30 12:42:47 +0530

    1

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+import time
+
+s_speed = 15
+window_x = 720
+window_y = 480
+
+black = pygame.color(0, 0, 0)
+white = pygame.color(255, 255, 255)
+red = pygame.color(255, 0, 0)
+green = pygame.color(0, 255, 0)
+blue = pygame.color(0, 0, 255)
+
+pygame.init()
+
+pygame.display.set_caption('Varun ke Saanp')
