ad2c69a3b0c8f22a5d2079b26e277c44331939fd
Varun Agarwal <165192602+Agarwalvarun23@users.noreply.github.com>
2025-06-30 12:45:47 +0530

    2

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ s_speed = 15
 window_x = 720
 window_y = 480
 
-black = pygame.color(0, 0, 0)
-white = pygame.color(255, 255, 255)
-red = pygame.color(255, 0, 0)
-green = pygame.color(0, 255, 0)
-blue = pygame.color(0, 0, 255)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
 
 pygame.init()
 
 pygame.display.set_caption('Varun ke Saanp')
+game_window = pygame.display.set_mode((window_x,window_y))
+fpd = pygame.time.Clock()
+
